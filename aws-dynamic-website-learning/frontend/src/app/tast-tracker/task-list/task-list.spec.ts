3720218274ace6ef06ae884acd16f1960c29140d
giancarlosierra <giancarlosierra2011@hotmail.com>
2026-07-29 16:16:56 -0500

    add changes

--- aws-dynamic-website-learning/frontend/src/app/tast-tracker/task-list/task-list.spec.ts
+++ aws-dynamic-website-learning/frontend/src/app/tast-tracker/task-list/task-list.spec.ts
@@ -1,22 +1,37 @@
+import { PLATFORM_ID } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-import { TaskList } from './task-list';
+import { TaskListComponent } from './task-list';
 
-describe('TaskList', () => {
-  let component: TaskList;
-  let fixture: ComponentFixture<TaskList>;
+describe('TaskListComponent', () => {
+  let component: TaskListComponent;
+  let fixture: ComponentFixture<TaskListComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskList],
+      imports: [TaskListComponent, HttpClientTestingModule],
+      providers: [{ provide: PLATFORM_ID, useValue: 'server' }],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TaskList);
+    fixture = TestBed.createComponent(TaskListComponent);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
     await fixture.whenStable();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not fetch tasks during server-side rendering', () => {
+    component.ngOnInit();
+
+    expect(component.tasks).toEqual([]);
   });
 });
--- aws-dynamic-website-learning/frontend/src/app/tast-tracker/task-list/task-list.ts
+++ aws-dynamic-website-learning/frontend/src/app/tast-tracker/task-list/task-list.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-task-list',
+  imports: [CommonModule],
   template: `
     <h2>Cloud Task Tracker</h2>
     <input #taskInput placeholder="Enter task..." />
@@ -16,16 +18,27 @@ export class TaskListComponent implements OnInit {
   tasks: any[] = [];
   private apiUrl = '/api/tasks'; // ALB will route /api to Node.js server
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: object
+  ) {}
 
-  ngOnInit() { this.fetchTasks(); }
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.fetchTasks();
+    }
+  }
 
   fetchTasks() {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+
     this.http.get<any[]>(this.apiUrl).subscribe(data => this.tasks = data);
   }
 
   addTask(title: string) {
-    if (!title) return;
+    if (!title || !isPlatformBrowser(this.platformId)) return;
     this.http.post(this.apiUrl, { title }).subscribe(() => this.fetchTasks());
   }
 }
